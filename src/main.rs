185d1697d956ab18978231e0e394470333a77638
thibaut.stachnick <thibaut.stachnick@kairios.fr>
2024-05-18 14:37:07 +0200

    change function to get a random hand from computer

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
 use std::collections::HashMap;
 use rand::Rng;
-fn get_random_hand()-> &'static str {
+use std::io::*;
+
+fn get_random_computer_hand()-> String {
     let random_hand = rand::thread_rng().gen_range(1..4);
-    let ia_hand : &str;
+    let mut ia_hand = String::new();
     match random_hand {
-        1=> ia_hand="rock",
-        2=>ia_hand = "papers",
-        3=>ia_hand = "scissors",
-        _ => ia_hand = "null"
+        1=> ia_hand=String::from("rock"),
+        2=>ia_hand = String::from("papers"),
+        3=>ia_hand = String::from("scissors"),
+        _ => ia_hand = String::from("error")
     }
-    ia_hand
+    println!("ia: {}",ia_hand);
+    return ia_hand
 }
 
-
-fn run(){
-    let mut match_points =  HashMap::from([
-        ("computer",0),
-        ("player",0)
-    ]);
-
-
-
-}
 
 fn main() {
-    let value = get_random_hand();
-    println!("{:?}", value);
+
 }
