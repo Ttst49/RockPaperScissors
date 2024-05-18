4bdda5fc2cbb7d87aa7a4079377f49d078eae512
thibaut.stachnick <thibaut.stachnick@kairios.fr>
2024-05-18 14:38:00 +0200

    add run function with all logic to make the game work

--- src/main.rs
+++ src/main.rs
@@ -42,6 +42,31 @@ fn ask_player_hand() -> String {
 }
 
 
+fn run(mut match_points: HashMap<&str, i32>, number_turns: i32){
+    let mut computer_hand = get_random_computer_hand();
+    computer_hand = computer_hand.trim().parse().unwrap();
+    let mut player_hand = ask_player_hand();
+    player_hand = player_hand.trim().parse().unwrap();
+    if computer_hand == player_hand {
+        println!("draw")
+    }
+    if computer_hand == "rock" && player_hand == "scissors"
+        || computer_hand == "scissors" && player_hand == "paper"
+        || computer_hand == "paper" && player_hand == "rock"
+    {
+        println!("computer played {}, it won",computer_hand);
+        match_points.insert("computer",match_points["computer"]+1);
+    }else {
+        println!("you played {}, you won",player_hand);
+        match_points.insert("player",match_points["player"]+1);
+    }
+    if match_points["computer"] == number_turns || match_points["player"] == number_turns {
+        println!("final score: \n {:?}",match_points)
+    }else {
+        run(match_points,number_turns)
+    }
+}
+
 fn main() {
 
 }
